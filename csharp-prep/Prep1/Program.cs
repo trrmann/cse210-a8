72f0295a6ba9c648b9f30e4bf07538b3d10f1a0f
trrmann <tracy.mann1@retailbusinessservices.com>
2023-04-17 16:49:40 -0400

    completed c# prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,33 @@
 using System;
 
+/**
+ * Learning Activity - C# Prep 1
+ * Tracy R. Mann
+ * CSE210-A8
+ * Bro. Nathan Parrish
+ * 4/17/2023
+ * 
+ * Assignment
+ *      Prompt the user for their first name. Then, prompt them for their
+ *   last name. Display the text back all on one line saying,
+ *   "Your name is last-name, first-name, last-name" as shown:
+ */
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // declare string vaiables
+        String first, last;
+        // output question for first name
+        Console.Write("What is your first name? ");
+        // store input to question for first name
+        first = Console.ReadLine();
+        // output question for last name
+        Console.Write("What is yout Last name? ");
+        // store input to question for last name
+        last = Console.ReadLine();
+        // output response as required:  "Your name is last-name, first-name, last-name"
+        Console.WriteLine($"\nYour name is {last}, {first} {last}.");
     }
 }
