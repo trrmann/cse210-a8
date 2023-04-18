c495035b511eb5ae778f9f5ac306b0a42bc16614
trrmann <tracy.mann1@retailbusinessservices.com>
2023-04-18 13:57:19 -0400

    copy prep4 comments to prep5 for model

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,5 +1,31 @@
 using System;
 
+/**
+ * Learning Activity - C# Prep 5
+ * Tracy R. Mann
+ * CSE210-A8
+ * Bro. Nathan Parrish
+ * 4/18/2023
+ * 
+ * Assignment
+ *      Ask the user for a series of numbers, and append each one to a list. Stop when they enter 0.
+ *      Once you have a list, have your program do the following:
+ *   
+ * Core Requirements
+ *      Work through these core requirements step-by-step to complete the program. Please don't skip
+ *   ahead and do the whole thing at once, because others on your team may benefit from building the
+ *   program up slowly.
+ *      1) Compute the sum, or total, of the numbers in the list.
+ *      2) Compute the average of the numbers in the list.
+ *      3) Find the maximum, or largest, number in the list.
+ * 
+ * Stretch Challenge
+ *      1) Have the user enter both positive and negative numbers, then find the smallest positive
+ *   number (the positive number that is closest to zero).
+ *      2) Sort the numbers in the list and display the new, sorted list. Hint: There are C# libraries
+ *   that can help you here, try searching the internet for them.
+ */
+
 class Program
 {
     static void Main(string[] args)
