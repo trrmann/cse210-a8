1ef8e486be1bcd67a7e6e4d314bc2a2e24a3df7e
trrmann <tracy.mann1@retailbusinessservices.com>
2023-04-25 19:16:05 -0400

    completeed step 5

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -5,4 +5,8 @@ public class Job
     public int _startYear;
     public int _endYear;
 
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
 }
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -8,14 +8,16 @@ class Program
         Job job1 = new Job();
         job1._company = "Microsoft";
         job1._jobTitle = "Software Engineer";
-        job1._startYear = 1999;
-        job1._endYear = 2001;
-        Console.WriteLine(job1._company);
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        //Console.WriteLine(job1._company);
+        job1.Display();
         Job job2 = new Job();
         job2._company = "Apple";
-        job2._jobTitle = "Software Engineer";
-        job2._startYear = 2001;
-        job2._endYear = 2005;
-        Console.WriteLine(job2._company);
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        //Console.WriteLine(job2._company);
+        job2.Display();
     }
 }
