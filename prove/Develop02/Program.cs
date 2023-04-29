476bb416a278af4e26c299b0a8884a6e0ecdab47
trrmann <tracy.mann1@retailbusinessservices.com>
2023-04-29 15:41:52 -0400

    added stretch plan notes

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -16,8 +16,8 @@ class Program
  * CSE210
  * Bro. Parrish
  * Section A8
- * 4/28/2023
- * v1.0
+ * 4/29/2023
+ * v1.1
  * 
  * What does the program do?
  *      For this assignment you will write a program to help people record the events
@@ -50,11 +50,20 @@ class Program
  *    2)  Contain at least two classes in addition to the Program class.
  *    3)  Demonstrate the principle of abstraction by using member variables and methods
  *    appropriately.
+ *  Stretch Requirements
+ *    1)  Add option to save and load JSON format.
+ *    2)  Read configuration for the application in JSON format from config file.
+ *    3)  Add checks for file extension assitance:  .csv for CSV file and .json for JSON file.
+ *    4)  Add counter for prompt questions used.
+ *    5)  Add last date used for prompt questions.
+ *    6)  Add distribution balancer for random selection of prompt questions.
+ *    7)  Add freeform prompt when all prompts have been used in the same day.
  *   
  * What user inputs does it have?
  *      1)  menu prompt response.
  *      2)  entry prompt response.
  *      3)  save/load filename prompt response.
+ *      4)  save/load file format prompt response.
  * What output does it produce?
  *      1)  display menu:
  *              1)  Add Journal entry.
@@ -77,11 +86,15 @@ class Program
  *      3)  request filename prompt.
  *              please enter the filename:
  *              >
- *      4)  report saving file.
+ *      4)  file format menu:
+ *              1)  CSV.
+ *              2)  JSON.
+ *              >
+ *      5)  report saving file.
  *              saving to file...
- *      5)  report loading file.
+ *      6)  report loading file.
  *              loading from file...
- *      6)  display journal entries.
+ *      7)  display journal entries.
  *              Journal:
  *              
  *                  {date} prompt: {prompt}
@@ -90,7 +103,7 @@ class Program
  *                  {date} prompt: {prompt}
  *                  {response}
  *                  ...
- *      5)  exit message
+ *      8)  exit message
  *              Thank you for using the Journal Program.
  * How does the program end?
  *      The user will select the exit option from the menu.
@@ -114,13 +127,26 @@ class Program
  *              entries List<Entry>
  *          Behaviors:
  *              get entry prompts
+ *              get entry prompt for entry
  *              prompt for entry
  *              read response
  *              add a new journal entry
  *              display all journal entries
  *              prompt for filename
+ *              prompt for file format
  *              save journal entries
  *              load journal entries
+ *      Prompt:
+ *          Responsibilty:
+ *              to hold information about the available prompts for jornal entries.
+ *          State:
+ *              value string
+ *              timesUsed int
+ *              lastUsed DateTime
+ *          Behaviors:
+ *              display prompt
+ *              parse json
+ *              write json
  *      Entry:
  *          Resonsibility:
  *              to hold and display information about an individual event.
@@ -132,6 +158,9 @@ class Program
  *              display entry
  *              parse csv
  *              write csv
+ *              parse json
+ *              write json
  *          
- *      I found the json format notes on stack overflow:  https://stackoverflow.com/questions/16921652/how-to-write-a-json-file-in-c
+ *      Notes:
+ *          I found the json format notes on stack overflow:  https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/how-to?pivots=dotnet-7-0
  */
