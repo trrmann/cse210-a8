809c4f9e284930cabba5dc4b981c914f1ba76c77
trrmann <tracy.mann1@retailbusinessservices.com>
2023-05-16 12:01:35 -0400

    Completed Step 3

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -4,4 +4,22 @@ public class Fraction
 {
 	private int _top;
     private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,6 +5,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        Fraction one = new Fraction();
+        Fraction whole = new Fraction(6);
+        Fraction fraction = new Fraction(6, 7);
+
         Console.WriteLine("Hello Learning03 World!");
     }
 }
